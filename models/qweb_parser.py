6afc0579cd611dfc6d1db6bd96625457542c6f3f
i.franchini <i.franchini@apuliasoftware.it>
2018-08-28 12:39:29 +0200

    [FIX] Report view and report count button

--- models/qweb_parser.py
+++ models/qweb_parser.py
@@ -26,7 +26,9 @@ class TechnicalReportsQweb(models.AbstractModel):
         if toDate and fromDate:
             date1 = datetime.strptime(toDate, '%Y-%m-%d %H:%M:%S')
             date2 = datetime.strptime(fromDate, '%Y-%m-%d %H:%M:%S')
-            return date1 - date2
+            result = date1 - date2
+            # result1 = datetime.strftime(result, '%H:%M:%S')
+            return result
 
 
 class ExternalReportsQweb(models.AbstractModel):
